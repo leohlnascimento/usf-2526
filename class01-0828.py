852a6cdebfaccf709ce53afb72b584f25a8acb70
leohlnascimento <69357934+leohlnascimento@users.noreply.github.com>
2025-09-02 20:59:06 +0000

    commit 09/02/25 1659

---
+++ class01-0828.py
@@ -0,0 +1,20 @@
+for i in range(1, 4):
+    name = str(input("Seu nome: "))
+    print(f"'Hello', {name}")
+
+# First Python class for Fall 25
+
+'''
+    01. Interpreter vs Compiler
+        - python uses an Interpreter, which reads the program line by line;
+        - single quotes will make the Interpreter expect a string;
+        - a Compiler reads the entire content before it executes any code
+          (that's why it's usually faster), while an Interpreter executes throughout
+          the reading of the code
+    
+    02. General Notes
+        - the backslash(\) is an escape character; it can be used, for example,
+          to print specific things that the Interpreter would maybe not understand
+          without the backslash
+
+'''
