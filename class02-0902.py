9d496c46c54dbe1603844fd73c8590efc9606a9e
leohlnascimento <69357934+leohlnascimento@users.noreply.github.com>
2025-09-04 21:23:54 +0000

    commit 09/04/25 1723

---
+++ class02-0902.py
@@ -0,0 +1,20 @@
+# Second Python class for Fall 25
+
+print("Hello", "Dados", sep="-", end=' oi ')
+print("Sim")
+
+'''
+    01. Escape sequences
+        character that allows us to format output
+        \n : newline
+        \t : tab
+        space, tab, or newline: whitespace
+
+    02. Variables
+        named spaces in memory
+        - identifier: names of components in program
+        # number = 45 -> initializing a variable (assignment)
+        print("number is", number) # echo printing
+        = is assignment operator
+        numSections -> camel naming
+'''
