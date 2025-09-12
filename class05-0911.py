3da1e94a48ecc564213f03a626dba7abdbd54f3a
leohlnascimento <69357934+leohlnascimento@users.noreply.github.com>
2025-09-12 18:43:11 +0000

    commit 09/12/25 1443

---
+++ class05-0911.py
@@ -0,0 +1,18 @@
+# test tuples and lists
+
+a = input().split()
+print(a)
+print(type(a))
+
+s1 = {"a", 3, 0, "hi"}
+print(type(s1))
+print(s1)
+
+# how does a set really works behind the scenes?
+s1.pop(2)
+print(s1)
+
+l1 = ['a', 'b', 'c']
+l1.pop()
+print(l1
+print = 'a'
