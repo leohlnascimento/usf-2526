805634f9f4ddd26d13d6fa6da02366dfc80a016a
leohlnascimento <69357934+leohlnascimento@users.noreply.github.com>
2025-09-18 18:39:14 +0000

    commit 09/18/25 1439

---
+++ RE01.py
@@ -0,0 +1,38 @@
+# a = int(input())
+# print('''"I've never been so scared!"''')
+
+# a = 2
+# b = 3
+# print('The momentum is', momentum := a*b, 'kg m/s.')
+# print(f'The momentum is {momentum := a*b} kg m/s.')
+
+# v = 1, 4 #tuple
+
+# a = 1000000
+# print(f"{a:,}")
+
+# a, b = input(), input()
+# a , b = input().split()
+# result = b + a
+# print(result)
+
+# print(a, b, sep='+-+')
+
+'''l = []
+print(l)
+print(type(l))'''
+
+'''import math
+a = math.log10(100)
+print(a)'''
+
+'''i = 1.23456
+
+print("{}".format(i, :3f}))'''
+
+list = [0, 1, 5, 8, 8, 2, 0, 3]
+list.remove(0)
+list.pop()
+print(list)
+print(set(list))
+print(type(set(list)))
