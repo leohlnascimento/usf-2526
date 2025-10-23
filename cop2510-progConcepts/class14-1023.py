b9e990fb27abc1efdc2fd8c5ad343b9594c863b0
leohlnascimento <69357934+leohlnascimento@users.noreply.github.com>
2025-10-23 22:16:24 +0000

    commit 10/23/25 1816

---
+++ cop2510-progConcepts/class14-1023.py
@@ -0,0 +1,31 @@
+# Week 9 Lecture 2
+
+# list comprehension with if statement
+list4 = [k+15 for k in range(10, 51, 10) if k<=30]
+print(list4)
+
+def teste(x):
+    return x+5
+teste(2)
+
+# dictionary, set, tuple comprehension also exist!
+# example of dictionary comprehension
+students = [('Alice', 85),
+            ('Bob', 70),
+            ('Cara', 92),
+            ('David', 87),
+            ('Eli', 90)]
+studentGrades = {name: ('A' if score >= 90 else 'B' if score >= 80 else 'C') 
+                 for name, score in students}
+print(studentGrades)
+
+# string slicing
+name = 'leo'
+print(name[::])
+print(name[::-1])
+
+# file input/output
+# write to a file
+with open('reminder.txt', mode='w') as outfile:
+     outfile.write('Update recitation session attendance.')
+     
